4fed292475d40cdb8ba2d7b80b2b56f6a68b4319
1300gang <149370274+1300gang@users.noreply.github.com>
2025-08-22 10:22:01 +0200

    Mise à jour reunionv2 - nouvelle version

--- server.js
+++ server.js
@@ -109,7 +109,7 @@ app.use(helmet({
 
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000,
-  max: process.env.NODE_ENV === 'production' ? 100 : 10000,
+  max: process.env.NODE_ENV === 'production' ? 100000 : 10000000,
   message: 'Trop de requêtes, réessayez plus tard'
 });
 
